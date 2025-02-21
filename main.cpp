6c29afeb69e5a9420f5fd8f7f22fea1af2e57a80
JeppeRonnow <Jepperw@gmail.com>
2025-02-21 11:47:56 +0100

    add delete function

--- main.cpp
+++ main.cpp
@@ -29,12 +29,14 @@ class TodoList {
 		cout << todolist.size() << endl;
 	}
 
+	// Delete task
 	void delete_task(int index) {
-
 		if (index >= 0 && index < todolist.size()) {
 			todolist.erase(todolist.begin() + index);
+		} 
+		else {
+			std::cout << "Task ikke fundet: " << index << std::endl;
 		}
-		std::cout << "Task ikke fundet: " << index << std::endl;
 	}
 
 };
@@ -45,6 +47,12 @@ int main() {
 
     // Create new task
     todo.create_task("TEST", "OnGoing");
+
+	// TEST PRINT
+    cout << todo.todolist.size() << endl;
+
+	// Delete task at index 0
+	todo.delete_task(0);
 
     // TEST PRINT
     cout << todo.todolist.size() << endl;
